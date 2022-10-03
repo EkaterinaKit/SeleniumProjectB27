7d951ac6fe59bef87fd18b53879a45465405d069
EkaterinaKit <kitaevak@gmail.com>
2022-10-02 21:09:02 -0400

    Changes added to RadioButton

--- src/test/java/com/cydeo/tests/day4_xpath_checkbox/RadioButton.java
+++ src/test/java/com/cydeo/tests/day4_xpath_checkbox/RadioButton.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class RadioButton {
@@ -29,6 +30,21 @@ public class RadioButton {
         WebElement greenRadioBtn = driver.findElement(By.id("green"));
         System.out.println(greenRadioBtn.getAttribute("disabled"));
         System.out.println(greenRadioBtn.isEnabled());//will print false because green button is disabled
+
+        List<WebElement> allColors = driver.findElements(By.name("color"));
+        System.out.println("allColors size is = " + allColors.size());
+
+        for( WebElement each : allColors){
+            if(each.isSelected()){
+                System.out.println("selected radio button is = " + each.getAttribute("id"));
+            }
+        }
+        System.out.println("-------------------------------------");
+        for (WebElement eachColor :  allColors){
+            eachColor.click();
+            System.out.println("eachColor is " + eachColor.isSelected());
+            System.out.println("eachColor is " + eachColor.isEnabled());
+        }
 
         driver.quit();
     }
